af76db96e6a862a19048c98a7979fe8c3e1f0a80
zysj <527522282@qq.com>
2018-02-25 10:35:00 +0800

    增加标题可点击功能、步骤完成监听事件； 增加注释

--- wizard-origin.js
+++ wizard-origin.js
@@ -9,12 +9,14 @@
 }(function($,global){
 
 var defaults = {
-    onNext:null,
-    onProgress:null,
-    onPrev:null,
-    headActiveClass:'wizard-head-active',
-    onError:null,
-    el:null
+    onNext:null,        //监听下一步按钮的函数
+    onProgress:null,    //监听执行步骤的函数
+    onPrev:null,        //监听上一步按钮的函数
+    onFinish:null,      //监听执行完成的函数
+    headActiveClass:'wizard-head-active',   //已执行步骤的激活类
+    onError:null,       //监听执行过程报错的函数
+    el:null,                
+    isHeadClick:false   //步骤标题是否可点击
 }
 
 function WizardOrigin(option){
@@ -33,15 +35,17 @@ function WizardOrigin(option){
 }
 
 WizardOrigin.prototype = {
+    //初始化函数
     init:function(){
         this.$heads.width((100/this.len)+'%');
         this.wizardChange(0,0);
         this.initButton();
+        this.option.isHeadClick && this.headClick();
     },
+    //标题和步骤内容变化的函数
     wizardChange:function(index,toIndex){
         toIndex = toIndex == null && index<this.len ? index+1 : toIndex;
         var that = this;
-        console.log(toIndex,index);
         var headActiveClass = this.option.headActiveClass;
         this.$heads.each(function(i,elem){
             var self = $(elem);
@@ -50,18 +54,16 @@ WizardOrigin.prototype = {
             }else{
                 !!headActiveClass && !self.hasClass(headActiveClass) && self.addClass(headActiveClass);
             }
+            that.$panels[i].style.display = i!=toIndex?'none':'block';
         });
-        if(index != toIndex){
-            this.$panels[index].style.display = 'none';
-        }
-        this.$panels[toIndex].style.display = 'block';
         return;
     },
+    //初始化上一步按钮以及下一步按钮的函数
     initButton:function(){
         var that = this;
         var nextFn = this.option.onNext;
         var prevFn = this.option.onPrev;
-        this.$next.on('click',function(e){
+        this.$next.length && this.$next.on('click',function(e){
             var event = e || window.event;
             var next = that.move(true);
             if(nextFn&&next){
@@ -70,7 +72,7 @@ WizardOrigin.prototype = {
                 next();
             }
         });
-        this.$prev.on('click',function(e){
+        this.$prev.length && this.$prev.on('click',function(e){
             var event = e || window.event;
             var prev = that.move(false);
             if(prevFn&&next){
@@ -81,6 +83,20 @@ WizardOrigin.prototype = {
         });
         return;
     },
+    //初始化标题点击函数
+    headClick:function(){
+        var that = this;
+        this.$heads.each(function(i){
+            var self = $(this);
+            self.on('click',function(){
+                if(i<=that.curIndex){
+                    that.$panels[i].style.display = 'block';
+                    $(that.$panels[i]).siblings('[wizard-origin-panel]').hide();
+                }
+            })
+        });
+    },
+    //步骤移动函数
     move:function(isNext){
         var that = this;
         var onProgress = this.option.onProgress;
@@ -92,7 +108,10 @@ WizardOrigin.prototype = {
                 return;
             };
             if(isNext){
-                if(index+1>that.len-1)return false;
+                if(index+1>that.len-1){
+                    that.option.onFinish && that.option.onFinish.call(that,index);
+                    return false;
+                }
                 toIndex = index+1;
                 that.wizardChange(index,toIndex);
             }else{
@@ -113,7 +132,6 @@ $.fn.WizardOrigin = function(option){
         WizardOrigin(option);
     })
 }
-
 
 $.WizardOrigin = function(option){
     WizardOrigin(option);
